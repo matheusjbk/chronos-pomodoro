ce0fc4178f546bc247716fe29734bebca09bb047
matheusjbk <matheusjbk@gmail.com>
2025-12-25 19:18:10 -0300

    chore: adding css modules example

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <h1>Olá, mundo!</h1>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
         neque facere in, nam aperiam mollitia earum quia unde atque cum saepe
