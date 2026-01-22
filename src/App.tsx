29d27b44c56eabe381fe02a0e891cfbe4ca44f7e
matheusjbk <matheusjbk@gmail.com>
2026-01-22 14:51:43 -0300

    feat: adding menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -13,7 +13,7 @@ export function App() {
       </Container>
 
       <Container>
-        <Heading>Menu</Heading>
+        <Menu />
       </Container>
     </>
   );
