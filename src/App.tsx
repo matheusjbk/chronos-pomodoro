af578f00c9dae127256a9cd86607e67451f7c7c2
matheusjbk <matheusjbk@gmail.com>
2026-01-22 14:10:50 -0300

    feat: creating container component and changing component folder and file pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { Trash2Icon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá, mundo!
-        <button>
-          <Trash2Icon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
-        neque facere in, nam aperiam mollitia earum quia unde atque cum saepe
-        voluptates? Maxime perferendis, magnam quia ipsa nobis dolores veniam.
-      </p>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
